c480101d7afb87b36f69426cfac23ae7ba84d4b1
ktyhanul@github.com <ktyhanul@gmail.com>
2015-02-11 19:04:20 +0900

    Commit test

--- src/com/fairytale/AdditionalStoryListActivity.java
+++ src/com/fairytale/AdditionalStoryListActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class AdditionalStoryListActivity extends Activity {
 
+//12345
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
