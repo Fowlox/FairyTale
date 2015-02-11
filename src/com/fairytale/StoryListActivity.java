f0fb98af16cdf00d8c7477b84a37e82ca89b87ca
OwlFox <coc1804@gmail.com>
2015-02-11 18:46:37 +0900

    Branch를 새로 팠다.

---
+++ src/com/fairytale/StoryListActivity.java
@@ -0,0 +1,13 @@
+package com.fairytale;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StoryListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
